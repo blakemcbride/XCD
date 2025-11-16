03f1ada67ec1d6ff492d5693be5531baade2b74d
Blake McBride <blake@mcbridemail.com>
2025-11-16 00:42:47 -0600

    Windows corrections

--- xcd.cmd
+++ xcd.cmd
@@ -1,6 +1,4 @@
 @echo off
-setlocal ENABLEEXTENSIONS
-
 rem xcd.cmd - Windows wrapper for xcd-win.exe
 rem Assumes xcd-win.exe is on the PATH and writes %TEMP%\xcd_target.txt.
 
@@ -11,16 +9,14 @@ if exist "%TEMPFILE%" del "%TEMPFILE%" >nul 2>&1
 
 rem Call the core with all arguments
 xcd-win %*
-set "ERR=%ERRORLEVEL%"
-
-if not "%ERR%"=="0" (
+if errorlevel 1 (
     rem Core reported an error; do not change directories
-    endlocal & exit /b %ERR%
+    goto :eof
 )
 
 if not exist "%TEMPFILE%" (
     rem Core did not produce a target file; nothing to do
-    endlocal & exit /b 1
+    goto :eof
 )
 
 set "TARGET_DIR="
@@ -29,10 +25,10 @@ del "%TEMPFILE%" >nul 2>&1
 
 if "%TARGET_DIR%"=="" (
     rem Empty target; nothing to do
-    endlocal & exit /b 1
+    goto :eof
 )
 
 cd /d "%TARGET_DIR%"
 
-endlocal
+:eof
 
